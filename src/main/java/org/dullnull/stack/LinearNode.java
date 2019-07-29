af8fe3b923b774dfc2bf4aaee58cd7d3ae366b4f
yuesheng <iyuesheng@hotmail.com>
2019-07-30 01:27:30 +0800

    The implement of ArrayStack and LinkedStack

---
+++ src/main/java/org/dullnull/stack/LinearNode.java
@@ -0,0 +1,72 @@
+package org.dullnull.stack;
+
+/**
+ * @author iisheng
+ * @date 2019/07/29 23:35:34
+ */
+public class LinearNode<T> {
+
+    /**
+     * 当前节点的下一个节点
+     */
+    private LinearNode<T> next;
+
+    /**
+     * 当前节点存储的元素
+     */
+    private T element;
+
+    /**
+     * 创建一个空节点
+     */
+    public LinearNode() {
+        next = null;
+        element = null;
+    }
+
+    /**
+     * 创建一个带特殊元素的节点
+     *
+     * @param element
+     */
+    public LinearNode(T element) {
+        next = null;
+        this.element = element;
+    }
+
+    /**
+     * 返回当前节点的下一个节点
+     *
+     * @return
+     */
+    public LinearNode<T> getNext() {
+        return next;
+    }
+
+    /**
+     * 设置当前节点的下一个节点
+     *
+     * @param node
+     */
+    public void setNext(LinearNode<T> node) {
+        next = node;
+    }
+
+    /**
+     * 返回节点中存储的数据
+     *
+     * @return
+     */
+    public T getElement() {
+        return element;
+    }
+
+    /**
+     * 设置节点中存储的数据
+     *
+     * @param element
+     */
+    public void setElement(T element) {
+        this.element = element;
+    }
+}
---
+++ src/main/java/org/dullnull/stack/LinkedStack.java
@@ -0,0 +1,64 @@
+package org.dullnull.stack;
+
+/**
+ * @author iisheng
+ * @date 2019/07/29 23:32:13
+ */
+public class LinkedStack<T> implements StackADT<T> {
+
+    /**
+     * 在栈中存储元素的数量
+     */
+    private int count;
+
+    /**
+     * 指向栈顶的指针
+     */
+    private LinearNode<T> top;
+
+    public LinkedStack() {
+        count = 0;
+        top = null;
+    }
+
+
+    @Override
+    public void push(T element) {
+        LinearNode<T> temp = new LinearNode<>(element);
+
+        temp.setNext(top);
+        top = temp;
+        count++;
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty.");
+        }
+
+        T result = top.getElement();
+        top = top.getNext();
+        count--;
+
+        return result;
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("The stack is empty.");
+        }
+        return top.getElement();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public int size() {
+        return count;
+    }
+}
